a2f74fcc580b87eb52c679e847f9833b0205f873
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-04-22 18:06:45 +0300

    12. Vue component registration (global/local) and Index component

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,16 @@ require('./bootstrap');
 import Vue from "vue";
 import VueRouter from "vue-router";
 import router from "./routes";
+import Index from "./Index";
 
 window.Vue = require('vue').default;
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('example-2', require('./components/Example2.vue').default);
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    components: {
+        "index": Index
+    }
 });
