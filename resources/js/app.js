f760cbb9562fbbd9816fa5d1c54efe08fcb350e9
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-04-22 17:59:39 +0300

    #11. HTML5 history mode in Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import router from "./routes";
 
 window.Vue = require('vue').default;
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-2', require('./components/Example2.vue').default);
 
 Vue.use(VueRouter);
 
