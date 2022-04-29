280a9bcaf0979937136b585f9de4ae810433f623
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-04-29 17:21:09 +0300

    19. Component props and prop types

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,19 +1,34 @@
 <template>
-    <div>
-        <bookable-list-item></bookable-list-item>
-        <bookable-list-item></bookable-list-item>
-        <bookable-list-item></bookable-list-item>
-        <bookable-list-item></bookable-list-item>
-        <bookable-list-item></bookable-list-item>
-    </div>
+  <div>
+    <bookable-list-item
+      item-title="The Title"
+      item-content="The Content"
+      :price="100000"
+    ></bookable-list-item>
+    <bookable-list-item
+      item-title="The Title 1"
+      item-content="The Content 1"
+      :price="100001"
+    ></bookable-list-item>
+    <bookable-list-item
+      item-title="The Title 2"
+      item-content="The Content 2"
+      :price="100002"
+    ></bookable-list-item>
+    <bookable-list-item
+      item-title="The Title 3"
+      item-content="The Content 3"
+      :price="100003"
+    ></bookable-list-item>
+  </div>
 </template>
 
 <script>
 import BookableListItem from "./BookableListItem";
 
 export default {
-    components:{
-        BookableListItem
-    }
-}
+  components: {
+    BookableListItem,
+  },
+};
 </script>
