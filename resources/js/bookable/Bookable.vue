3f06656c9b1e511041c4ec013d5c84d42d03e97d
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-05-01 17:59:04 +0300

    39. Vue router parameters

---
+++ resources/js/bookable/Bookable.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">xxx</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bookable: null,
+    };
+  },
+  created() {
+    axios.get(`/api/bookables/${this.$route.params.id}`).then((response) => this.bookable = response.data);
+  },
+};
+</script>
