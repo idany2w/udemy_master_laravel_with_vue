d6391128bd7e10927b7f0f14bd27ac1fa9373e4f
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-05-02 14:36:31 +0300

    44. Resource controllers

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('bookables', [App\Http\Controllers\Api\BookableController::class, 'index']);
-Route::get('bookables/{id}', [App\Http\Controllers\Api\BookableController::class, 'show']);
+Route::apiResource('bookables', App\Http\Controllers\Api\BookableController::class)
+    ->except([])
+    ->only(['index', 'show']);
