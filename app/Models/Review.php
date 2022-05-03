6778b9b240f15167df7d3e67858afe3bcd968ff1
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-05-03 20:12:55 +0300

    61. Review model relationships (one-to-one, one-to-many)

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,16 @@ class Review extends Model
 {
     use HasFactory;
 
+    public function bookable()
+    {
+        return $this->belongsTo(Bookable::class);
+    }
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
+
     // И как правильно? Надо подумать
     public $autoincrement = false;
     public $incrementing = false;
@@ -16,7 +26,7 @@ class Review extends Model
     {
         return false;
     }
-    
+
     public function getKeyType()
     {
         return 'string';
