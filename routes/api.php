adc2faa13d69ed3f79a72e5fadd321e089c3e92f
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-05-03 20:46:02 +0300

    63. Reviews controller (using latest() query scope)

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\BookableAvailabilityController;
-use App\Models\Bookable;
+use App\Http\Controllers\Api\BookableReviewController;
+use App\Http\Controllers\Api\BookableController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,8 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('bookables', App\Http\Controllers\Api\BookableController::class)
+Route::apiResource('bookables', BookableController::class)
     ->except([])
     ->only(['index', 'show']);
 Route::get('bookables/{bookable}/availability', BookableAvailabilityController::class)
     ->name('bookables.availability.show');
+Route::get('bookables/{bookable}/reviews', BookableReviewController::class)
+    ->name('bookables.reviews.index');
+
