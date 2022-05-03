f34ccf10d151a83dd90e34272847e0b5a81607cc
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-05-03 13:55:26 +0300

    52. Single Action Controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\BookableAvailabilityController;
 use App\Models\Bookable;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('bookables', App\Http\Controllers\Api\BookableController::class)
     ->except([])
     ->only(['index', 'show']);
+Route::get('bookables/{bookable}/availability', BookableAvailabilityController::class)
+    ->name('bookables.availability.show');
