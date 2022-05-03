b8cdaa008f1571fa27dd7c65ac42b62ade58a0b7
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-05-03 14:30:47 +0300

    54. Eloquent Local Query Scopes

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bookable;
 use Illuminate\Http\Request;
 
 class BookableAvailabilityController extends Controller
@@ -13,11 +14,14 @@ class BookableAvailabilityController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke($id, Request $request)
     {
         $data = $request->validate([
             'from' => 'required|date_format:Y-m-d|after_or_equal:now',
             'to' => 'required|date_format:Y-m-d|after_or_equal:from',
         ]);
+
+        $bookable = Bookable::findOrFail($id);
+        return $bookable->bookings()->betweenDates($data['from'], $data['to'])->get();
     }
 }
--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Booking extends Model
 {
@@ -16,5 +17,10 @@ class Booking extends Model
     public function bookable()
     {
         return $this->belongsTo(Bookable::class);
+    }
+
+    
+    public function scopeBetweenDates(Builder $query, $from, $to){
+        return $query->where('to', '>=', $from)->where('from', '<=', $to);
     }
 }
