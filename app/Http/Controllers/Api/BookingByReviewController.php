e64bb18a3701708886631ca5c4e77e6d46574cbe
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-05-06 07:17:59 +0300

    79. Creating Resource classes for booking

--- app/Http/Controllers/Api/BookingByReviewController.php
+++ app/Http/Controllers/Api/BookingByReviewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BookingByReviewShowResource;
 use App\Models\Booking;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,6 @@ class BookingByReviewController extends Controller
      */
     public function __invoke($review_key, Request $request)
     {
-        return Booking::findByReviewKey($review_key) ?? abort(404);
+        return new BookingByReviewShowResource(Booking::findByReviewKey($review_key)) ?? abort(404);
     }
 }
