9d336a009cb414a83109082fc0b85fff82e1fff2
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-05-14 23:11:52 +0300

    133. Understanding authentication with Sanctum

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
+        $this->call(UsersTableSeeder::class);
         $this->call(BookableTableSeeder::class);
         $this->call(BookingsTableSeeder::class);
         $this->call(ReviewsTableSeeder::class);
--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,6 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource('bookables', BookableController::class)
     ->except([])
     ->only(['index', 'show']);
