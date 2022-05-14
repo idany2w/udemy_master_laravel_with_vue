de0793ff28c68b8882d172393dc057c378259234
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-05-14 17:50:31 +0300

    112. Keeping basket state in localStorage

--- resources/js/store.js
+++ resources/js/store.js
@@ -18,7 +18,9 @@ export default {
         removeFromBasket(state, payload) {
             state.basket.items = state.basket.items.filter(item => item.bookable.id != payload)
         },
-
+        setBasket(state, payload) {
+            state.basket = payload
+        },
     },
     actions: {
         setLastSearch(context, payload) {
@@ -30,7 +32,20 @@ export default {
             if (lastSearch) {
                 context.commit('setLastSearch', JSON.parse(lastSearch))
             }
-        }
+
+            const basket = localStorage.getItem('basket');
+            if (basket) {
+                context.commit('setBasket', JSON.parse(basket))
+            }
+        },
+        addToBasket({commit, state}, payload) {
+            commit('addToBasket', payload)
+            localStorage.setItem('basket', JSON.stringify(state.basket));
+        },
+        removeFromBasket() {
+            commit('removeFromBasket', payload)
+            localStorage.setItem('basket', JSON.stringify(state.basket));
+        },
     },
     getters: {
         itemsInBasket: (state) => state.basket.items.length,
